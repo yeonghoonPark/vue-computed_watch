a4387a6bd9c07748b1ace1d6039a1b9170348d95
yeonghoon Park <huklmnj2@naver.com>
2022-08-03 23:34:53 +0900

    difference of between computed & watch

---
+++ src/views/Computed_WatchView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Forward direction : {{ message }}</h1>
+    <hr />
+    <h1>Reverse direction : {{ reverseMessage() }}</h1>
+    <p>{{ date }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      message: '안세요',
+      date: Date.now(),
+    };
+  },
+  // computed: {
+  //   reverseMessage: function () {
+  //     return this.message.split('').reverse().join('');
+  //   },
+  // },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    reverseMessage: function () {
+      return this.message.split('').reverse().join('');
+    },
+  },
+};
+</script>
---
+++ src/views/WatchVeiw.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ count }}</h1>
+    <br />
+    <p>{{ text }}</p>
+    <button @click="count++">Increase count</button>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      count: 0,
+      text: 'Before the change',
+    };
+  },
+  watch: {
+    count(newValue, oldValue) {
+      this.text = oldValue + '에서' + newValue + '(으)로 변경';
+    },
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
